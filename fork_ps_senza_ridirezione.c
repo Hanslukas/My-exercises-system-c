d5b9f5dbaec8076d19a1940f2e71d12ec82fe057
Gianluca La Manna <gl.lamanna@gmail.com>
2013-11-10 15:25:05 +0100

    Create fork_ps_senza_ridirezione.c

---
+++ fork_ps_senza_ridirezione.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+main()
+{
+    int childpid;
+    int m;
+    if ( (childpid = fork()) == -1)
+    {
+	      printf("Errore nella fork\n");
+	      exit(1);
+    }
+    else
+    {
+	      switch(childpid)
+	      {
+	          case 0: printf("figlio: pid figlio=%d, pid padre=%d\n", getpid(), getppid());
+			              system("ps lax");
+			              exit(0);
+			              break;
+
+			      case -1: printf("Errore\n");
+				            break;
+
+			      default: printf("padre: pid figlio=%d, pid padre=%d\n", childpid, getpid());
+				             system("ps lax");
+				             exit(0);
+				             break;
+		   
+	      }
+    }
+}
